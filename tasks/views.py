5a83965df717bff7a5ce06de30c56f782b6f92a7
Esneidr <eacordobam@gmail.com>
2024-07-17 22:47:14 -0500

    2 commit

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return HttpResponse('Hello World')
+def home(request):
+    return render(request, 'home.html')
+
+def singup(request):
+
+    if request.method == 'GET':
+        return render(request, 'singup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username already exists')
+        return HttpResponse('Password do not macth')
+
+
